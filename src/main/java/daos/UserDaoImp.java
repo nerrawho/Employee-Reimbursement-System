971eaa3b3e7c29e3d6a84d42cc43b27d9d2be1fa
Mimi-AB <seunabimbola98@gmail.com>
2022-02-05 12:49:08 -0500

    UserDAO update

--- src/main/java/daos/UserDaoImp.java
+++ src/main/java/daos/UserDaoImp.java
@@ -1,4 +1,39 @@
 package daos;
 
-public class UserDaoImp {
+import models.User;
+
+import java.util.List;
+
+public class UserDaoImp implements UserDao
+{
+
+    @Override
+    public void createUser(User u)
+    {
+
+    }
+
+    @Override
+    public List<User> readAllUser()
+    {
+        return null;
+    }
+
+    @Override
+    public User readPersonById(int id)
+    {
+        return null;
+    }
+
+    @Override
+    public void updateUser(User u)
+    {
+
+    }
+
+    @Override
+    public void deleteUser(User u)
+    {
+
+    }
 }
