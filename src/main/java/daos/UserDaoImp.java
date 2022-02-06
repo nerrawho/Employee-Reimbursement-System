c414a66b6a46c6b01a1065fcc803cd7c93b61fa6
Mimi-AB <seunabimbola98@gmail.com>
2022-02-05 19:14:40 -0500

    DAO update

--- src/main/java/daos/UserDaoImp.java
+++ src/main/java/daos/UserDaoImp.java
@@ -4,10 +4,7 @@ import models.User;
 import models.UserRole;
 import utils.ConnectionUtil;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -53,18 +50,51 @@ public class UserDaoImp implements UserDao
     @Override
     public User readPersonById(int id)
     {
-        return null;
+        User u = new User();
+        String sql = "SELECT * FROM users where id=" + id;
+        try (Connection c = ConnectionUtil.getConnection();
+             PreparedStatement ps = c.prepareStatement(sql);)
+        {
+            ResultSet rs = ps.executeQuery(sql);
+            while (rs.next())
+            {
+                u = new User (rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), UserRole.values()[rs.getInt(6)]);
+            }
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        } return u;
     }
 
     @Override
     public void updateUser(User u)
     {
+        String sql = "UPDATE users SET first_name= " + u.getFirst() +", " + "last_name= " + u.getLast() + ", "
+                + "username = " + u.getUsername() + ", " + "password= " + u.getPassword() + ", "
+                + "email='" + u.getEmail() + ", " + "Role= " + (u.getRole().ordinal()) + " WHERE id=" + u.getUserID();
+        try (Connection c = ConnectionUtil.getConnection();
+             Statement s = c.createStatement();)
+        {
+            s.executeUpdate(sql);
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public void deleteUser(User u)
     {
+        String sql = "DELETE FROM users WHERE id=" + u.getUserID();
+        try (Connection c = ConnectionUtil.getConnection();
+             Statement s = c.createStatement();)
+        {
+            s.executeUpdate(sql);
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
 
     }
 }
